5f410808f6764dcbfebdd6c330936ab86143e0da
Teepheh-git <teepheh@yahoo.com>
2022-06-01 00:31:54 +0100

    progress

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -8,7 +8,8 @@ import { TextInput } from "react-native-paper";
 
 const Login = () => {
 
-  const [text, setText] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
 
   return (
@@ -36,41 +37,39 @@ const Login = () => {
             d="M160.194 7.9621L9.69437 146.346C3.51601 152.027 0 160.036 0 168.429V289.742C0 306.31 13.4314 319.742 30 319.742H331C347.569 319.742 361 306.31 361 289.742V168.429C361 160.036 357.484 152.027 351.306 146.346L200.806 7.9621C189.326 -2.59321 171.674 -2.59321 160.194 7.9621Z"
             fill="white"
           />
-
-          <View style={styles.loginBox}>
-            <Text style={styles.loginTxt}>Login</Text>
-
-            <TextInput
-              label="Email"
-              value={text}
-              underlineColor={COLORS.black}
-              style={{
-                backgroundColor:"white",
-                // color: "black"
-              }}
-              onChangeText={text => setText(text)}
-            />
-
-            <TextInput
-              label="Password"
-              value={text}
-              underlineColor={COLORS.black}
-
-              style={{
-                backgroundColor:"white",
-                // color: "black"
-              }}
-              onChangeText={text => setText(text)}
-            />
-          </View>
-
-
         </Svg>
 
-        <View style={styles.bottomBox}>
+        <View style={styles.loginBox}>
+          <Text style={styles.loginTxt}>Login</Text>
 
+          <TextInput
+            label="Email"
+            value={email}
+            underlineColor={COLORS.black}
+            style={{
+              backgroundColor: "white",
+              // color: "black"
+            }}
+            onChangeText={text => setEmail(text)}
+          />
 
+          <TextInput
+            label="Password"
+            value={password}
+            underlineColor={COLORS.black}
+
+            style={{
+              backgroundColor: "white",
+              // color: "black"
+            }}
+            onChangeText={text => setPassword(text)}
+          />
         </View>
+
+
+        <View style={styles.bottomBox} />
+
+
       </View>
 
     </View>
@@ -110,6 +109,7 @@ const styles = StyleSheet.create({
     height: SIZES.height * 0.5,
   },
   loginBox: {
+    position: "absolute",
     width: SIZES.width * 0.9,
     height: SIZES.height * 0.2,
     // backgroundColor: "rgba(0,0,0,0.2)",
